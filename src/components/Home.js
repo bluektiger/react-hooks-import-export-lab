bea63426866c2c587dd0a777ea1a2aa3374be1d6
mark <markkixkithkim@gmail.com>
2024-04-28 10:14:14 +0300

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import { city } from "../data/user";
+import { username } from "../data/user";
 function Home() {
   return (
-    <div id="home">
+    <div id="home" >
       <h1>
         {username} is a Web Developer from {city}
       </h1>
     </div>
   );
 }
+
+export default Home;
